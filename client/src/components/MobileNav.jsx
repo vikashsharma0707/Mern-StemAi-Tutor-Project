1b916e0ec34a91fcaf05f74b210f5a43e8117e64
vikash sharma <vikashtiwari9893@gmail.com>
2026-06-11 18:42:52 +0530

    code

--- client/src/components/MobileNav.jsx
+++ client/src/components/MobileNav.jsx
@@ -5,7 +5,7 @@ export default function MobileNav({ user, onMenuToggle }) {
   return (
     <header className="mobile-nav">
       <button className="mobile-nav-menu" onClick={onMenuToggle}><Menu size={24} /></button>
-      <div className="mobile-nav-brand"><GraduationCap size={22} /><span>STEMAI Tutor project</span></div>
+      <div className="mobile-nav-brand"><GraduationCap size={22} /><span>STEMAI Tutor</span></div>
       <div className="mobile-nav-user"><User size={16} /><span>{user?.name?.split(' ')[0] || 'User'}</span></div>
     </header>
   );
