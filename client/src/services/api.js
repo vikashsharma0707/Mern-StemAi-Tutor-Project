9471e98321618ad77e912b6bd0976fc14cdff0da
vikash sharma <vikashtiwari9893@gmail.com>
2026-06-12 17:53:40 +0530

    add server live api in client

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,14 +1,48 @@
+// import axios from 'axios';
+
+// const API = axios.create({
+//   baseURL: '/api',
+// });
+
+// API.interceptors.request.use((config) => {
+//   const token = localStorage.getItem('token');
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// API.interceptors.response.use(
+//   (res) => res,
+//   (err) => {
+//     if (err.response?.status === 401) {
+//       localStorage.removeItem('token');
+//       localStorage.removeItem('user');
+//       if (window.location.pathname !== '/login') {
+//         window.location.href = '/login';
+//       }
+//     }
+//     return Promise.reject(err);
+//   }
+// );
+
+// export default API;
+
+
+
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: '/api',
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
@@ -18,10 +52,12 @@ API.interceptors.response.use(
     if (err.response?.status === 401) {
       localStorage.removeItem('token');
       localStorage.removeItem('user');
+
       if (window.location.pathname !== '/login') {
         window.location.href = '/login';
       }
     }
+
     return Promise.reject(err);
   }
 );
